2f6af803ba8ad760051a87a20af8cf66636a477c
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2024-11-08 10:37:44 -0400

    a

---
+++ src/main/java/com/mycompany/arbolgot/ArbolGOT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arbolgot;
+
+/**
+ *
+ * @author gabri
+ */
+public class ArbolGOT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
