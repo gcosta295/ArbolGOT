eff211c7f0a507c8217cc03e77fc36915d8d4d29
astv06 <156257557+astv06@users.noreply.github.com>
2024-11-08 10:50:34 -0400

    test git

--- src/main/java/com/mycompany/arbolgot/ArbolGOT.java
+++ src/main/java/com/mycompany/arbolgot/ArbolGOT.java
@@ -11,6 +11,6 @@ package com.mycompany.arbolgot;
 public class ArbolGOT {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("test!");
     }
 }
