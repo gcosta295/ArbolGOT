91dfdaaecd1c0b40a2c929a52bae8659cc321fa1
astv06 <156257557+astv06@users.noreply.github.com>
2024-11-18 15:48:25 -0400

    merge

--- src/main/java/com/mycompany/arbolgot/TreeGOT.java
+++ src/main/java/com/mycompany/arbolgot/TreeGOT.java
@@ -11,17 +11,20 @@ public class TreeGOT {
 //utilizar el programa con escala al 100% (configuracion)
 
     public static void main(String[] args) {
-        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+        
         ReadJSON M = new ReadJSON();
         M.abrirArchivo();
         List persons = M.Parse();
         persons = M.Arbol(persons);
         M.arreglarHijos(persons);
+        System.setProperty("org.graphstream.ui", "swing");
         Tree arbol = new Tree();
         arbol.Graph(persons);
         Clicks clicks = new Clicks();
         clicks.setGraph(arbol.getGraph());
         clicks.setViewer(arbol.getViewer());
+        clicks.setPersons(persons);
         clicks.Clicks1();
     }
 }
