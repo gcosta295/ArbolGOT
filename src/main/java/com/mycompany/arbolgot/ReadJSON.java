6c57518f9a15fc853caba19fef460fc9a5dcaf46
astv06 <156257557+astv06@users.noreply.github.com>
2024-11-18 16:00:00 -0400

    arreglo merges

--- src/main/java/com/mycompany/arbolgot/ReadJSON.java
+++ src/main/java/com/mycompany/arbolgot/ReadJSON.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.arbolgot;
-//commment
+//commment2
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
